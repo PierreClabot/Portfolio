683ee02ac740dcc1f9d4412d4e685e80b6747e31
Pierre Clabot <pierre.clabot@gmail.com>
2023-04-07 17:44:50 +0200

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+class App{
+    constructor(){
+
+    }
+
+    main(){
+        const mouse = new Mouse();
+    }
+}
+
+const app = new App();
+app.main();
---
+++ src/utils/hub.js
@@ -0,0 +1,21 @@
+class Mouse{
+    constructor(){
+        this.domMouse = document.querySelector(".mouse");
+        this.icons = document.querySelectorAll(".icon i");
+        document.addEventListener("mousemove",(e)=>{
+            this.domMouse.style.display = "block"
+            this.domMouse.style.left = `${e.clientX}px`;
+            this.domMouse.style.top = `${e.clientY}px`;
+        })
+
+        this.icons.forEach(icon=>{
+            icon.addEventListener("mousemove",function(){
+                this.classList.add("fa-bounce")
+            })
+            icon.addEventListener("mouseleave",function(){
+                // console.log(this.classList.value.search("fa-bounce"))
+                this.classList.remove("fa-bounce");
+            })
+        })
+    }
+}
